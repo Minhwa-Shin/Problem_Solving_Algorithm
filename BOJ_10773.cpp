aed6e248a60273840d0649ba4fe0061ed5a3d6df
Minhwa-Shin <65149183+Minhwa-Shin@users.noreply.github.com>
2021-03-08 12:11:48 +0900

    Update BOJ_10773.cpp

--- BOJ_10773.cpp
+++ BOJ_10773.cpp
@@ -1,35 +1,30 @@
 #include <iostream>
+#include <algorithm>
+#include <stack>
 using namespace std;
 
-#define MAXN ((int)1e5)
 int K;
-int sp=0;
-int stk[MAXN+10];
 
-void push(int n){stk[++sp]=n;};
-bool empty(){return sp==0;}
-int size(){return sp;}
-int top(){
-    if(!empty()) return stk[sp];
-    return -1;
+int solve()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> K;
+
+    stack<int> stk;
+    int num, total=0;
+    for(int i=0;i<K;++i){
+        cin >> num;
+        if(num==0 && !stk.empty()){
+            total-=stk.top(); stk.pop();
+        }else{
+            total+=num; stk.push(num);
+        }
+    }
+    return total;
 }
-void pop(){--sp;}
 
 int main()
 {
-    int n;
-    cin >> K;
-    for(int i=0;i<K;i++){
-        cin >> n;
-        if(n!=0) push(n);
-        else pop();
-    }
-
-    int sum=0;
-    while(!empty()){
-        sum+=top(); pop();
-    }
-    printf("%d",sum);
-
-    return 0;
+    cout << solve() << "\n";
 }
